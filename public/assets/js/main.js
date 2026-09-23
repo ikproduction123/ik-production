77b122a2459446a0385328edc4f520d2aff2b889
ikproduction123 <indrayulianto949@gmail.com>
2026-09-24 03:47:56 +0700

    Create IK-Pro landing page

---
+++ public/assets/js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const faqItems = document.querySelectorAll(".faq-item");
+
+    faqItems.forEach((item) => {
+        const button = item.querySelector(".faq-question");
+
+        button.addEventListener("click", () => {
+            item.classList.toggle("active");
+        });
+    });
+});
